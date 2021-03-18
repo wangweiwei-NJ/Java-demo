ad2bbb9b6133006fbbb4ccd1f8d78c3a43a2eb75
Wang Weiwei <wangweiwei@njust.edu.cn>
2021-03-18 17:23:50 +0800

    The first version of demo.

--- src/demo/WeekDayCalculator.java
+++ src/demo/WeekDayCalculator.java
@@ -1,6 +1,5 @@
 package demo;
 
-import java.time.LocalDate;
 import java.util.Arrays;
 
 /**
@@ -22,16 +21,5 @@ public class WeekDayCalculator {
         dayOfMonth[1] += y % 400 == 0 || (y % 4 == 0 && y % 100 > 0) ? 1 : 0;
         sum += Arrays.stream(dayOfMonth).limit(m - 1).reduce(0, Integer::sum);
         return y < 1998 || m < 1 || m > 12 || d < 1 || d > dayOfMonth[m - 1] ? "Invalid date input!" : days[sum % 7];
-    }
-
-    public static void main(String[] args) {
-//        for (int k = 1998; k < 1002010; k++) {
-//            if (!calcDay(k, 5, 31).equals(LocalDate.of(k, 5, 31).getDayOfWeek().toString())) {
-//                System.out.println(k);
-//            }
-//        }
-
-        System.out.println(calcDay(1999, 3, 13));
-        System.out.println(LocalDate.of(1999, 3, 13).getDayOfWeek());
     }
 }
